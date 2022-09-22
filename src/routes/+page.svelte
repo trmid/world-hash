b4fe1ed66eb98c12263b915ad3083e0545074a2a
Ncookiez <ncukiert@gmail.com>
2022-09-22 00:07:15 -0300

    Main Structure Done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,13 @@
   // Initializations:
   // :3
 
-  /* JS Goes Here */
+  // Function to scroll to bottom of page:
+  const scrollDown = () => {
+    const mainElement = document.querySelector('main');
+    if(mainElement) {
+      mainElement.scrollTop += window.innerHeight;
+    }
+  }
 	
 </script>
 
@@ -14,17 +20,56 @@
 
 <!-- SvelteKit Dynamic Header -->
 <svelte:head>
-	<title>WorldHash</title>
+	<title>World Hash</title>
 	<meta name="description" content="The toolkit for sharing Minecraft worlds through ENS and IPFS." />
 </svelte:head>
 
-<!-- HTML Goes Here -->
-<span>Hello World Hash</span>
+<!-- Search Section -->
+<section id="searchSection">
+  section 1
+  <img src="/images/portal.png" alt="Nether Portal" id="portal" on:click={scrollDown}>
+</section>
+
+<!-- Share Section -->
+<section id="shareSection">
+  section 2
+</section>
 
 <!-- #################################################################################################### -->
 
 <style>
 
-	/* CSS Goes Here */
+	section {
+    position: relative;
+    height: 100vh;
+    width: 100%;
+    padding: 2em;
+    scroll-snap-align: start;
+    overflow: hidden;
+    isolation: isolate;
+  }
+
+  #searchSection {
+    background: url('/images/minecraftBG.png');
+    background-repeat: no-repeat;
+    background-position: top right;
+  }
+
+  #portal {
+    position: absolute;
+    bottom: 78px;
+    right: 268px;
+  }
+
+  #portal:hover {
+    filter: drop-shadow(0 0 1.5em var(--secondary-color));
+    cursor: pointer;
+  }
+
+  #shareSection {
+    background: url('/images/netherBG.png');
+    background-repeat: no-repeat;
+    background-position: top right;
+  }
 	
 </style>
