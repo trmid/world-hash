b5b5d5bbe7500cbb5e7025388c0b3db1a3e133d2
trmid <trmidpoint@gmail.com>
2022-09-21 22:43:53 -0400

    Add list and launch endpoints

--- src/routes/world/list/+server.ts
+++ src/routes/world/list/+server.ts
@@ -1,3 +1,40 @@
-import { error } from '@sveltejs/kit';
- 
-/** @type {import('./$types').RequestHandler} */
+import * as fs from "fs/promises";
+import { join } from "path";
+import type { RequestHandler } from './$types';
+import { error, json } from '@sveltejs/kit';
+import { getSavesDir } from "$lib/server/minecraft";
+
+/**
+ * Fetches a list of local worlds from disk.
+ * 
+ * @returns Response with local world list
+ */
+export const GET: RequestHandler = async () => {
+
+  // Get saves directory:
+  const savesDir = await getSavesDir();
+  if(!savesDir) throw error(500, "Missing saves directory...");
+
+  // Get world folders:
+  const worlds: { name: string, imageSrc: string }[] = [];
+  const dirContents = await fs.readdir(savesDir);
+  for(const filename of dirContents) {
+
+    // Check if world dir exists:
+    const stat = await fs.stat(join(savesDir, filename));
+    if(stat.isDirectory()) {
+
+      // Check if dir contains a level.dat file:
+      const levelDatStat = await fs.stat(join(savesDir, filename, "level.dat"));
+      if(levelDatStat.isFile()) {
+        
+        // Push world to list:
+        worlds.push({
+          name: filename,
+          imageSrc: join(savesDir, filename, "icon.png")
+        });
+      }
+    }
+  }
+  return json({ worlds });
+};
