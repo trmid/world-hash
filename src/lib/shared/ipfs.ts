f90466c8f5bd64a7d7586a484d1b20b704e30e6b
trmid <trmidpoint@gmail.com>
2022-09-23 13:34:01 -0400

    Add IPFS proxy gateway

--- src/lib/shared/ipfs.ts
+++ src/lib/shared/ipfs.ts
@@ -1,17 +1,19 @@
-import { PUBLIC_IPFS_API } from "$env/static/public";
-import { join } from "path";
-
+import { PUBLIC_IPFS_API, PUBLIC_IPFS_GATEWAY } from "$env/static/public";
 
 export async function apiCall(method: string, endpoint: string, params?: URLSearchParams) {
-  return await fetch(`${PUBLIC_IPFS_API}${join("/", endpoint)}${params ? "?" + params.toString() : ""}`, { method });
+  const url = `${PUBLIC_IPFS_API}${endpoint}${params ? "?" + params.toString() : ""}`;
+  console.log(`IPFS API Call: ${method} ${url}`);
+  return await fetch(url, { method });
 }
 
-export async function ipfsFetch(cid: string, path?: string) {
-  return await apiCall('GET', `/ipfs/${cid}${path ? join("/", path) : ""}`);
+export function ipfsFetch(cid: string, path?: string) {
+  const url = `${PUBLIC_IPFS_GATEWAY}/ipfs/${cid}/${path ? path : ""}`;
+  console.log(`Requesting resource from IPFS: GET ${url}`);
+  return fetch(url);
 }
 
 export async function isValidCID(cid: string) {
   const res = await apiCall('POST', `/api/v0/cid/format`, new URLSearchParams({ arg: cid }));
   const json = await res.json();
-  return !!json.ErrorMsg;
+  return !json.ErrorMsg;
 }
---
+++ src/routes/ipfs/[cid]/[...path]/+server.ts
@@ -0,0 +1,22 @@
+import type { RequestHandler } from './$types';
+import { error } from '@sveltejs/kit';
+import { ipfsFetch, isValidCID } from '$lib/shared/ipfs';
+
+/**
+ * GET
+ * 
+ * A text proxy gateway for debugging IPFS requests.
+ */
+export const GET: RequestHandler = async ({ params }) => {
+
+  // Check if CID is valid:
+  if(!await isValidCID(params.cid)){
+    throw error(400, "invalid CID");
+  }
+
+  // Fetch resource:
+  const res = await ipfsFetch(params.cid, params.path || undefined);
+
+  // Return result
+  return res;
+}
