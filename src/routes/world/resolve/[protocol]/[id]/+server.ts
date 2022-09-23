346fcd1dc45cc05d042426fdac4025bfc5d0e319
trmid <trmidpoint@gmail.com>
2022-09-23 16:46:03 -0400

    Add basic resolution functionality

--- src/routes/world/resolve/[protocol]/[id]/+server.ts
+++ src/routes/world/resolve/[protocol]/[id]/+server.ts
@@ -1,11 +1,13 @@
 import type { RequestHandler } from './$types';
 import { error, json } from '@sveltejs/kit';
 import { ethers } from 'ethers';
+import { ipfsFetch, isValidCID } from '$lib/shared/ipfs';
+import { isValidWorldInfo, type WorldInfo } from '$lib/shared/world';
 
 /**
- * Fetches a list of local worlds from disk.
+ * Resolves a world catalog from a CID or ENS name.
  * 
- * @returns Response with local world list
+ * @returns JSON response with world catalog.
  */
 export const GET: RequestHandler = async ({ params }) => {
 
@@ -16,7 +18,7 @@ export const GET: RequestHandler = async ({ params }) => {
   if(params.protocol.match(/^ens$/i)) {
     
     // Fetch content CID from ENS:
-    const provider = new ethers.providers.JsonRpcProvider();
+    const provider = new ethers.providers.JsonRpcProvider(process.env.PUBLIC_ETHEREUM_RPC_URL || "https://cloudflare-eth.com");
     const resolver = await provider.getResolver(params.id);
     const worldHashContent = await resolver?.getText("minecraft");
     if(!worldHashContent) throw error(404, `Could not resolve ENS minecraft field for name: ${params.id}`);
@@ -31,7 +33,24 @@ export const GET: RequestHandler = async ({ params }) => {
   }
 
   // Check if CID is valid:
-  
+  if(!await isValidCID(cid)) throw error(400, `invalid CID: ${cid}`);
 
-  throw error(500);
+  // Resolve JSON directory:
+  let worldCatalog: { worlds: Record<string, WorldInfo> } = { worlds: {} };
+  try {
+    worldCatalog = await (await ipfsFetch(cid)).json();
+  } catch(err) {
+    console.error(err);
+    throw error(500, `failed to resolve JSON from CID: ${cid}`);
+  }
+  
+  // Validate world info:
+  for(const [cid, info] of Object.entries(worldCatalog.worlds)) {
+    if(!await isValidCID(cid) && isValidWorldInfo(info)) {
+      throw error(500, "resolved invalid world info");
+    }
+  }
+
+  // Return worlds:
+  return json({ cid, ...worldCatalog });
 };
--- src/routes/world/resolve/[protocol]/[id]/[worldHashCID]/+server.ts
+++ src/routes/world/resolve/[protocol]/[id]/[worldHashCID]/+server.ts
@@ -1,14 +1,51 @@
 import type { RequestHandler } from './$types';
 import { error, json } from '@sveltejs/kit';
+import { GET as GETWorldCatalog } from '../+server';
+import { apiCall, isValidCID } from '$lib/shared/ipfs';
 
 /**
- * Fetches a list of local worlds from disk.
- * 
- * @returns Response with local world list
+ * Begins a resolution of a world with the given worldHashCID from the given world 
+ * catalog information and pins it to the local IPFS node as well as copies it to
+ * the minecraft saves folder.
  */
-export const GET: RequestHandler = async ({ params }) => {
+export const PUT: RequestHandler = async (req) => {
 
+  // Get params:
+  const { params } = req;
 
+  // Resolve the world catalog:
+  const worldCatalogRes = await GETWorldCatalog(req);
+  if(worldCatalogRes.status != 200) {
+    return worldCatalogRes;
+  }
+  const worldCatalog = await worldCatalogRes.json();
+
+  // Check to see if the world hash CID exists in the catalog:
+  const worldInfo = worldCatalog.worlds[params.worldHashCID];
+  if(!worldInfo) throw error(500, "could not find world hash CID in catalog");
+
+  // Check if valid CID:
+  if(!await isValidCID(params.worldHashCID)) throw error(500, `invalid CID: ${params.worldHashCID}`);
+
+  // Copy it to node files:
+  const args = new URLSearchParams();
+  args.append("arg", `/ipfs/${params.worldHashCID}`);
+  args.append("arg", `/world-hash/${params.id}/${worldCatalog.cid}/${params.worldHashCID}`);
+  args.set("parents", "true");
+  const cpRes = await apiCall("POST", "/api/v0/files/cp", args);
+  if(cpRes.status !== 200) return cpRes;
+
+  // Copy it to .minecraft/saves:
+  // TODO
 
   throw error(500);
 };
+
+/**
+ * Gets the current status of the resolution process.
+ */
+export const GET: RequestHandler = async({ params }) => {
+
+  // TODO: provide progress of resolution
+  return json({ status: 'n/a', progress: 0 });
+}
