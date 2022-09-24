3a13c94478b4f8f2382209e36de82962bf8af7ae
trmid <trmidpoint@gmail.com>
2022-09-24 15:03:20 -0400

    future-proof typing

--- src/lib/shared/world.ts
+++ src/lib/shared/world.ts
@@ -1,10 +1,23 @@
+export type WorldCatalog = Record<string, WorldInfo>
+
+export interface MinecraftJSON {
+  worlds: WorldCatalog
+}
+
 export interface WorldInfo {
   name: string
   timestamp: number // seconds since UNIX epoch
   creator: string // ENS name (or other identifier in the future)
 }
 
-export function isValidWorldInfo(info: WorldInfo) {
+export function isValidWorldCatalog(catalog: WorldCatalog): catalog is WorldCatalog {
+  for(const key in catalog) {
+    if(!isValidWorldInfo(catalog[key])) return false;
+  }
+  return true;
+}
+
+export function isValidWorldInfo(info: WorldInfo): info is WorldInfo {
   return (
     typeof info === 'object' &&
     typeof info.name === 'string' &&
