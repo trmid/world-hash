4e9d16ec493a12249623976df2061049e1a28c34
trmid <trmidpoint@gmail.com>
2022-09-25 11:03:24 -0400

    Change default ports

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		port: 25556
+	},
+	preview: {
+		port: 25557
+	}
 };
 
 export default config;
